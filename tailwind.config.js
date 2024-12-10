79009c84ec0eb0c6e2e9074e90c06ee8bd8697b3
Valery Reddevotion <linkolnsid228@gmail.com>
2024-12-10 14:23:26 +0300

    1st commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,30 @@
 /** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
+import { fontFamily } from "tailwindcss/defaultTheme";
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        color: {
+          1: "#FFFFFF",
+          2: "#C5F82A",
+          3: "#141414",
+        },
+        n: {
+          1: "#141414",
+          2: "#1f1f1f",
+          3: "#333333",
+        }
+      },
+      fontFamily: {
+        sans: ["var(--font-inter)", ...fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }
