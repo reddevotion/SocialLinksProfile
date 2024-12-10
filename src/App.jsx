e64cacf7131017df80a1154c1f9ee867094e6dd5
Valery Reddevotion <linkolnsid228@gmail.com>
2024-12-10 11:32:01 +0300

    vite+react+tailwind

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+function App() {
+
+  return (
+    <>
+      
+  Jessica Randall
+  London, United Kingdom
+  "Front-end developer and avid reader."
+
+  GitHub
+  Frontend Mentor
+  LinkedIn
+  Twitter
+  Instagram
+  
+  <div class="attribution hidden">
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+    Coded by <a href="#">Your Name Here</a>.
+  </div>
+    </>
+  )
+}
+
+export default App
